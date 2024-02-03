c884316437919d18497333d72f9b80b2d210afeb
kmert0@outlook.com <kmert0@outlook.com>
2024-02-03 11:38:09 +0300

    Adding the "New Project" Component & A Reusable "Input" Component

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -4,9 +4,11 @@ export default function ProjectsSidebar() {
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
-      <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-        + New Project
-      </button>
+      <div>
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+          + New Project
+        </button>
+      </div>
       <ul></ul>
     </aside>
   );
