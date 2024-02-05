91a5d598d3583d7d1a1fec55e4e425c7505ff88b
kmert0@outlook.com <kmert0@outlook.com>
2024-02-05 22:26:37 +0300

    Handling Project Creation & Updating the UI

--- src/App.jsx
+++ src/App.jsx
@@ -24,20 +24,23 @@ function App() {
       };
       return {
         ...prevState,
-        projects:[...prevState.projects, newProject],
-      }
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
     });
   }
-  console.log(projectsState);
   let content;
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject}/>;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
